7df7e091754f37b1bb7ac8438b2d0a7aa245fede
reuixiy <reuixiy@gmail.com>
2021-11-13 17:43:23 +0800

    chore: remove styled-jsx and clean up codebase

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
+    scrollRestoration: true,
     concurrentFeatures: true,
     serverComponents: true,
     urlImports: ['https://cdn.skypack.dev'],
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import '@/styles/index.css'
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
