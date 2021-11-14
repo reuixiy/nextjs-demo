647f2dc770eafefcb77a98011aafcf6f574656b9
reuixiy <reuixiy@gmail.com>
2021-11-14 17:01:55 +0800

    style: rename to global.css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import '@/styles/index.css'
+import '@/styles/global.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
