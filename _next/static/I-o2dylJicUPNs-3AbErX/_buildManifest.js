9acc49af4f66fbb271efb25229fd6df0521e2e55
reuixiy <reuixiy@users.noreply.github.com>
2022-03-05 15:44:09 +0000

    deploy: 1b4aa5bf2c4c859119469cb3201536fea01b7152

---
+++ _next/static/I-o2dylJicUPNs-3AbErX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1e47732332b6b80a.js"],"/_error":["static/chunks/pages/_error-f93ea34e061b9bc7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
